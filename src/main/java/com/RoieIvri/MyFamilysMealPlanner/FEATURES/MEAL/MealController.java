960412fc640705d64d2c83be0e932a5ecd10c029
roie123 <roieivri123@gmail.com>
2023-02-01 20:09:38 +0200

    Meade the MealController @CrossOrigin

--- src/main/java/com/RoieIvri/MyFamilysMealPlanner/FEATURES/MEAL/MealController.java
+++ src/main/java/com/RoieIvri/MyFamilysMealPlanner/FEATURES/MEAL/MealController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/meal")
 @RequiredArgsConstructor
+@CrossOrigin
 public class MealController implements GodController<Meal> {
 
     @Autowired
