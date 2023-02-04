68b44f45eeeed8fa2b5916c83052d0715469e47f
roie123 <roieivri123@gmail.com>
2023-02-04 19:28:14 +0200

    Made the user Controller @CrossOrigin

--- src/main/java/com/RoieIvri/MyFamilysMealPlanner/FEATURES/USER/UserController.java
+++ src/main/java/com/RoieIvri/MyFamilysMealPlanner/FEATURES/USER/UserController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@CrossOrigin
 public class UserController implements GodController<User>{
     @Autowired
     private final UserService userService;
